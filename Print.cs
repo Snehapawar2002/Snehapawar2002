0606a033c5051d45ab8243c04a93a7309d19370c
Snehapawar2002 <99063383+Snehapawar2002@users.noreply.github.com>
2022-02-17 08:31:16 +0530

    Create Print.cs

---
+++ Print.cs
@@ -0,0 +1,21 @@
+- 👋 Hi, I’m @Snehapawar2002
+- 👀 I’m interested in ...
+- 🌱 I’m currently learning ...
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...
+
+<!---
+Snehapawar2002/Snehapawar2002 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
+using System;using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ namespace ConsoleApp1
+{    
+class Program   
+ {static void Main(string[] args)
+{For(int i=0; i<=5; i++)
+{ Console.WriteLine("read one, each one,teach one");
+ Console.ReadLine();}}    }}
